8da29da925b63142fc7249c3d3cf407a193ab2d5
edl13 <ed.l.1324@gmail.com>
2015-02-06 20:23:43 -0500

    aw;oejf

--- app/src/main/java/com/droplet/droplet/MainActivity.java
+++ app/src/main/java/com/droplet/droplet/MainActivity.java
@@ -1,6 +1,4 @@
 package com.droplet.droplet;
-import android.app.ActionBar;
-import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Handler;
 import android.os.StrictMode;
@@ -13,16 +11,17 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AbsListView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.ProgressBar;
-import android.widget.Toast;
+
+import com.firebase.client.ChildEventListener;
+import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.Query;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class MainActivity extends ActionBarActivity implements AbsListView.OnScrollListener {
@@ -63,12 +62,12 @@ public class MainActivity extends ActionBarActivity implements AbsListView.OnScr
                 double latitude = locationGet.getLatitude();
                 double longitude = locationGet.getLongitude();
                 Log.wtf("WHEEEEE", "LONG " + longitude + " LAT " + latitude);
-                //Toast.makeText(getApplicationContext(), "Your Location is - \nLat: " + latitude + "\nLong: " + longitude, Toast.LENGTH_LONG).show();
                 Droplet drop = new Droplet(longitude, latitude, "Edward", "ayy lmao");
                 vals.add(""+longitude+" " + latitude);
-                FirebaseAsync runFirebase = new FirebaseAsync();
+                PushFirebaseAsync runFirebase = new PushFirebaseAsync();
                 runFirebase.execute(drop);
-
+                GetFirebaseAsync getFirebase = new GetFirebaseAsync(longitude, latitude);
+                getFirebase.execute(1);
             }
         });
 
@@ -138,7 +137,7 @@ public class MainActivity extends ActionBarActivity implements AbsListView.OnScr
         }
     };
 
-    private class FirebaseAsync extends AsyncTask<Droplet , Void, Void> {
+    private class PushFirebaseAsync extends AsyncTask<Droplet , Void, Void> {
 
 
         @Override
@@ -146,9 +145,77 @@ public class MainActivity extends ActionBarActivity implements AbsListView.OnScr
             Firebase.setAndroidContext(getApplicationContext());
             rootRef = new Firebase("https://dropletserver.firebaseio.com/");
             Firebase dropRef = rootRef.child("Drops");
-            Map<String, Droplet> dropMap = new HashMap<String, Droplet>();
-            dropMap.put(drops[0].getHash(), drops[0]);
-            dropRef.push().setValue(dropMap);
+//            Map<String, Droplet> dropMap = new HashMap<String, Droplet>();
+//            dropMap.put(drops[0].getHash(), drops[0]);
+            dropRef.push().setValue(drops[0]);
+            return null;
+        }
+
+    }
+
+
+    private class GetFirebaseAsync extends AsyncTask<Integer , Void, Void> {
+
+        private double longi;
+        private double lati;
+
+        public GetFirebaseAsync(double longIn, double latIn){
+            super();
+            longi = longIn;
+            lati = latIn;
+        }
+
+
+
+        // returns [lowerLong, upperLong, lowerLat, upperLat
+        private double[] getBounds(){
+
+            //Assuming 1 deg lat is 111111 meters, 1 degree long is 111111*cos(lat) meters
+            //800 meters is...
+            double dLat = 0.0072;
+            double dLong = 800/111111*Math.cos(Math.toRadians(dLat));
+
+            double[] result = { longi - dLong, longi + dLong, lati - dLat, lati + dLat  };
+            return result;
+        }
+
+        @Override
+        protected Void doInBackground(Integer... i) {
+            Firebase.setAndroidContext(getApplicationContext());
+            rootRef = new Firebase("https://dropletserver.firebaseio.com/");
+            Firebase dropRef = rootRef.child("Drops");
+            double[] boundsArr = getBounds();
+            Query locQuery = dropRef.orderByChild("longitude").startAt(boundsArr[0]).endAt(boundsArr[1]).orderByChild("latitude").startAt(boundsArr[2]).endAt(boundsArr[3]);
+
+            locQuery.addChildEventListener(new ChildEventListener() {
+                @Override
+                public void onChildAdded(DataSnapshot snapshot, String previousChild) {
+                    Map<String, Object> value = (Map<String, Object>)snapshot.getValue();
+                    Log.wtf("WLUIRAHWEF" , "long: " + value.get("longitude") + " lati: " + value.get("latitude"));
+                    System.out.println(snapshot.getKey() + " was " + value.get("height") + " meters tall");
+                }
+
+                @Override
+                public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+                }
+
+                @Override
+                public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+                }
+
+                @Override
+                public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+                }
+
+                @Override
+                public void onCancelled(FirebaseError firebaseError) {
+
+                }
+
+            });
             return null;
         }
 
